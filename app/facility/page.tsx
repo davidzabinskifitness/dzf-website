a6e5132728b31550b1dcd12e4b144aca4b0949a8
davidzabinskifitness <davidzabinskifitness@gmail.com>
2026-09-09 12:47:27 -0500

    Update Gym hero photo

--- app/facility/page.tsx
+++ app/facility/page.tsx
@@ -1,4 +1,5 @@
-import type { Metadata } from "next";
+facility-exterior.jpg
+  gym-hero.pngimport type { Metadata } from "next";
 import Link from "next/link";
 import Image from "next/image";
 import GarageMap from "@/components/GarageMap";
